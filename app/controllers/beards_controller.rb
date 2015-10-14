dc841a914ed91ce207ad6a1149a6e786c1901570
yelena nedelko & lindsay culver <student@epicodus.com>
2015-10-14 15:49:10 -0700

    add edit beardability

--- app/controllers/beards_controller.rb
+++ app/controllers/beards_controller.rb
@@ -20,6 +20,19 @@ class BeardsController < ApplicationController
     end
   end
 
+  def edit
+    @beard = Beard.find(params[:id])
+  end
+
+  def update
+    @beard = Beard.find(params[:id])
+    if @beard.update(beard_params)
+      redirect_to beard_path
+    else
+      render :edit
+    end
+  end
+
   private
   def beard_params
     params.require(:beard).permit(:name, :rating, :image_url)
--- spec/features/add_beard_spec.rb
+++ spec/features/add_beard_spec.rb
@@ -19,3 +19,14 @@ describe "adding a beard process" do
     expect(page).to have_content 'You have these errors:'
   end
 end
+
+describe "updating a beard" do
+  it "edits a beard by given info" do
+    beard = Beard.create(:name => 'Lumberjack', :rating => '6', :image_url => '/images/lumberjack.png')
+    visit beard_path(beard)
+    click_on 'Edit Beard'
+    fill_in 'Rating', :with => '10'
+    click_on 'Update Beard'
+    expect(page).to have_content '10'
+  end
+end
