1ceb0664cf243c191a35b344505cb43dc6ecb717
lindsay culver & yelena nedelko <student@epicodus.com>
2015-10-15 10:57:50 -0700

    add ability to add a review for each beard

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def new
+    @beard = Beard.find(params[:beard_id])
+    @review = @beard.reviews.new
+  end
+
+  def create
+    @beard = Beard.find(params[:beard_id])
+    @review = @beard.reviews.new(review_params)
+    if @review.save
+      redirect_to beard_path(@review.beard)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:user, :text)
+  end
+end
---
+++ spec/features/add_review_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "adding a review process" do
+  it "adds a new review" do
+    beard = Beard.create(:name => 'Lumberjack', :rating => '6', :image_url => '/images/lumberjack.png')
+    visit beard_path(beard)
+    click_on "Add a review"
+    fill_in 'User', :with => 'Bob'
+    fill_in 'Text', :with => 'Love it!'
+    click_on 'Create Review'
+    expect(page).to have_content 'Love it!'
+  end
+end
